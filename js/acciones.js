21a38ce481599aed204afe88275920cfa93cd990
Silvia Vazquez <azul_xv@hotmail.com>
2014-08-19 10:21:39 -0500

    02 Prueba 02
    Se corrige navigator p

--- js/acciones.js
+++ js/acciones.js
@@ -3,11 +3,11 @@
 $(document).ready(function(e){	
 	document.addEventListener("deviceready",function(){
     $('#beep').tap(function(){
-		Navigator.notificacion.beep(1);
+		navigator.notificacion.beep(1);
 	}); // tap Beep
 	
 	$('#vibrar').tap(function(){
-		Navigator.notificacion.vibrate(1000);
+		navigator.notificacion.vibrate(1000);
 	}
 	);// tap Vibrar
 	},false); //deviceready
