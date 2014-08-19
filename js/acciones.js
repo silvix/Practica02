28831ebd879f6ceb26e563882b6372c6f145cebe
Silvia Vazquez <azul_xv@hotmail.com>
2014-08-19 10:31:53 -0500

    03 Practica
    notification

--- js/acciones.js
+++ js/acciones.js
@@ -3,11 +3,11 @@
 $(document).ready(function(e){	
 	document.addEventListener("deviceready",function(){
     $('#beep').tap(function(){
-		navigator.notificacion.beep(1);
+		navigator.notification.beep(1);
 	}); // tap Beep
 	
 	$('#vibrar').tap(function(){
-		navigator.notificacion.vibrate(1000);
+		navigator.notification.vibrate(1000);
 	}
 	);// tap Vibrar
 	},false); //deviceready
